da549a9ca6a47dd1c16b82aa702129829dfa9d23
Tomasz <tmax0135@gmail.com>
2020-02-29 13:33:53 +0100

    Delete j_write_string.ino

--- led_controller/j_write_string.ino
+++
@@ -1,9 +0,0 @@
-char* rmvstr(char str[], char toRemove[], char *result){
-    byte needed = strlen(str) - strlen(toRemove);
-    for (int i = 0; i < needed; i++) {
-      result[i] = str[strlen(toRemove) + i];
-    }
-    // Add string termination letter
-    result[needed] = '\0';
-    return result;
-}
