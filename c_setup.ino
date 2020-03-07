ae53374b4a1375eb99e147d6438a0cc8652165a8
Tomasz <tmax0135@gmail.com>
2020-03-07 07:34:13 +0100

    Update c_setup.ino

--- c_setup.ino
+++ c_setup.ino
@@ -5,7 +5,7 @@ void setup() {
   bt.begin(serialSpeed);
   //Initialize the strips
   LED.begin(); // Without this led's won't turn on
-  Serial.println(F("LED receiver started"));
+//   Serial.println(F("LED receiver started"));
   
   LED.setBrightness(100);
   LED.Fade(FADE, LED.Color(0,0,0), LED.Color(0,0,0), 1, 1);
